08ef8d2c2439ecabb35a63566973d8f732788b87
Arjan Aswal <arjanaswal09@gmail.com>
2020-04-24 17:55:49 +0530

    Email Scraping Script

---
+++ EmailScraper.sh
@@ -0,0 +1,54 @@
+clear
+figlet EmailScraper
+
+echo press 1 to scrape default list and 2 to scrape fresh list
+
+read choice
+
+if [ $choice = '1' ]
+then
+
+arr[0]='Blogger'
+arr[1]='Writer'
+arr[2]='Actor'
+
+
+elif [ $choice = '2' ]
+then
+echo Enter the profiles and write stop when finished
+
+
+IFS=''
+
+while read line
+do
+if [ "$line" = 'stop' ]
+then
+break
+else
+    arr=("${arr[@]}" "$line")
+fi
+done
+else
+echo Error! press only the given characters
+fi
+
+
+write_file () {
+for profile in ${arr[@]}
+do
+
+# VPN Code from Command Line and comment the 2 lines below
+echo please change your IP address and then press 'enter' to resume
+read resume
+
+sudo node app.js "$profile"
+sudo node extract.js "$profile"
+cat ./TEMP__DUMP/List_"$profile"*.txt > ./Result/"$profile".txt
+rm -rf ./TEMP__DUMP/List_"$profile"*.txt
+rm -rf ./TEMP__DUMP/List_"$profile"*.json
+echo Compiled Text file at /Result/"$profile".txt
+done
+}
+
+write_file
---
+++ README.md
@@ -0,0 +1,17 @@
+A simple nodejs powered bash script that queries Google for specific keywords and then scrapes the results and outputs the email addresses associated with it. The results will be saved in a text file in Results folder.
+
+Please don't use it to abuse or spam.
+
+Installation : 
+
+git clone https://github.com/ArjanAswal/EmailScraper
+cd EmailScraper
+npm install
+bash EmailScraper.sh
+
+Prerequisites:
+node js 
+npm
+VPN
+
+Note: IP address needs to be changed after a specific query otherwise Google will block your queries and you will get no results. The script will prompt you for it. Please don't use it for spamming.
